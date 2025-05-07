9d4fcfd51e2142416945cd4347589b2d40597127
anhpt16 <nhatanh16122002@gmail.com>
2025-05-07 17:16:46 +0700

    init "res_app"

---
+++ src/test/java/com/anhpt/res_app/ResAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhpt.res_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
